693e1e952383c4836e00cf4abfc07d729c7a3e86
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-09-28 09:33:32 -0500

    Visualizacion
    Se organiza como sera la visualizacion de los componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './components/main/layout/layout.component';
+import { CharactersComponent } from './components/characters/characters.component';
+import { RickandmortyComponent } from './components/rickandmorty/rickandmorty.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    component : LayoutComponent
+  },
+  {
+    path : 'characters',
+    component : CharactersComponent
+  },
+  {
+    path : 'rickandmorty',
+    component : RickandmortyComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
