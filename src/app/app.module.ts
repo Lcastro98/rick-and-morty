11f68fa0a29b23074c20345e89e71fd67ec56aa3
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-09-28 09:15:39 -0500

    Componentes
    Se organizan los componentes del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/main/header/header.component';
+import { MenuComponent } from './components/main/menu/menu.component';
+import { LayoutComponent } from './components/main/layout/layout.component';
+import { FooterComponent } from './components/main/footer/footer.component';
+import { CharactersComponent } from './components/characters/characters.component';
+import { RickandmortyComponent } from './components/rickandmorty/rickandmorty.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    LayoutComponent,
+    FooterComponent,
+    CharactersComponent,
+    RickandmortyComponent
   ],
   imports: [
     BrowserModule,
