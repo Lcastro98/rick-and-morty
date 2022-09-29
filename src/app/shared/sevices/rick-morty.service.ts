5739ee0a54dad5534af8f53269aca378d7f60588
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-09-29 15:33:51 -0500

    Creación del servicio y la interfaz a utilizar para consultar la api

---
+++ src/app/shared/sevices/rick-morty.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Character } from '../interfaces/Character';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RickMortyService {
+
+  constructor(private _http: HttpClient) { }
+  
+  searchCharacters(query = '', page = 200):Observable<Character[]> {
+    const filter = `${environment.baseAPI}/?name=${query}&page=${page}`;
+    return this._http.get<Character[]>(filter);
+  }
+
+  getDetails(id: number) {
+    return this._http.get<Character>(`${environment.baseAPI}/${id}`)
+    .pipe(catchError((err) => err));
+  }
+}
