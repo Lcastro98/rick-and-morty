2883becd7d6ae4e30f24644cb392625f4984c23c
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-09-29 15:39:49 -0500

    Se agrega el componente para listar los personajes

---
+++ src/app/components/rickandmorty/rickandmorty-list/rickandmorty-list.component.ts
@@ -0,0 +1,101 @@
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject, OnInit, } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, take } from 'rxjs/operators';
+import { Character } from 'src/app/shared/interfaces/Character';
+import { RickMortyService } from 'src/app/shared/sevices/rick-morty.service';
+
+type RequestInfo = {
+  next: string | null;
+};
+
+@Component({
+  selector: 'app-rickandmorty',
+  templateUrl: './rickandmorty-list.component.html',
+  styleUrls: ['./rickandmorty-list.component.css']
+})
+export class RickandmortyListComponent implements OnInit {
+
+  characters: Character[] = [];
+
+  info: RequestInfo = {
+    next: null,
+  };
+
+  showGoUpButton = false;
+
+  private pageNum = 1;
+
+  private query: string = '';
+
+  private hideScrollHeight = 200;
+
+  private showScrollHeight = 500;
+
+  constructor(
+    @Inject(DOCUMENT) private document:Document,
+    private characterSvc: RickMortyService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {
+    this.onUrlChanged();
+  }
+
+  ngOnInit(): void {
+    this.getCharactersByQuery();
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll():void {
+    const yOffSet = window.pageYOffset;
+    if ((yOffSet || this.document.documentElement.scrollTop || this.document.body.scrollTop) > this.showScrollHeight) {
+      this.showGoUpButton = true;
+    } else if (this.showGoUpButton && (yOffSet || this.document.documentElement.scrollTop || this.document.body.scrollTop) < this.hideScrollHeight) {
+      this.showGoUpButton = false;
+    }
+  }
+
+  onScrollDown():void{
+    if (this.info.next) {
+      this.pageNum++;
+      this.getDataFromService();
+    }
+  }
+
+  onScrollTop():void{
+    this.document.body.scrollTop = 0; // Safari
+    this.document.documentElement.scrollTop = 0; // Other
+  }
+
+  private onUrlChanged(): void {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.characters = [];
+        this.pageNum = 1;
+        this.getCharactersByQuery();
+      });
+  }
+
+  private getCharactersByQuery(): void {
+    this.route.queryParams.pipe(take(1)).subscribe((params) => {
+      this.query = params['q'];
+      this.getDataFromService();
+    })
+  }
+
+  private getDataFromService(): void {
+    this.characterSvc
+      .searchCharacters(this.query, this.pageNum)
+      .pipe(take(1))
+      .subscribe((res: any) => {
+        if (res?.results?.length) {
+          const { info, results } = res;
+          this.characters = [...this.characters, ...results];
+          this.info = info;
+        } else {
+          this.characters = [];
+        }
+      })
+    }
+}
